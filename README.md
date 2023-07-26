58c37f40538a6b54278bc4bd85784f4c83400a42
IreneChicca <irene.chicca@outlook.com>
2023-07-26 16:27:50 +0200

    Third ex

--- README.md
+++ README.md
@@ -6,3 +6,9 @@ Chiedi all'utente un primo numero, poi un secondo e genera un numero casuale com
 # TRACCIA DUE
 
 Dare la possibilità di inserire due parole. Verificare che le due parole abbiano la stessa lunghezza. Se hanno la stessa lunghezza, stamparle entrambe altrimenti stampare la più lunga delle due.
+
+
+# TRACCIA TRE 
+
+
+Crea un array vuoto e chiedi all'utente un numero da inserire nell'array. Continua a chiedere i numeri all'utente e a inserirli nell'array fino a quando la somma degli elementi è minore di 50.
--- main.js
+++ main.js
@@ -21,32 +21,49 @@
 // Dare la possibilità di inserire due parole. Verificare che le due parole abbiano la stessa lunghezza. Se hanno la stessa lunghezza, stamparle entrambe altrimenti stampare la più lunga delle due.
 
 
-const firstW = prompt("Inserisci una parola")
+// const firstW = prompt("Inserisci una parola")
 
-const secondW = prompt("Inserisci un'altra parola")
+// const secondW = prompt("Inserisci un'altra parola")
 
-let firstL = firstW.length
-let secondL = secondW.length
+// let firstL = firstW.length
+// let secondL = secondW.length
 
-if (firstL == secondL) {
+// if (firstL == secondL) {
 
-    console.log(firstW)
-    console.log(secondW)
+//   console.log(firstW)
+// console.log(secondW)
+
+
+//}
+
+
+// else if (firstL > secondL) {
+
+// console.log(firstW)
+
+// }
+
+// else if (firstL < secondL) {
+
+// console.log(secondW)
+// }
+
+// Crea un array vuoto e chiedi all'utente un numero da inserire nell'array. Continua a chiedere i numeri all'utente e a inserirli nell'array fino a quando la somma degli elementi è minore di 50.
+
+const myArr = [];
+
+let sum = 0;
+
+while (sum < 50) {
+
+    const newNum = parseInt(prompt("inserisci un numero"));
+    sum += newNum;
+
+    myArr.push(newNum)
+
+    console.log(newNum)
+    console.log(sum)
+
 
 
 }
-
-
-else if (firstL > secondL) {
-
-    console.log(firstW)
-
-}
-
-else if (firstL < secondL) {
-
-    console.log(secondW)
-}
-
-
-
